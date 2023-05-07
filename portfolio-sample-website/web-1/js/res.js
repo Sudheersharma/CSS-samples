c5d8a53da8f2b201ad43aa23c679893bec4d9a5c
Sudheer Sharma <108560715+Sudheersharma@users.noreply.github.com>
2023-05-07 22:14:12 +0530

    css-projects-upload

---
+++ portfolio-sample-website/web-1/js/res.js
@@ -0,0 +1,14 @@
+burger = document.querySelector('.burger')
+navbar = document.querySelector('.navbar')
+rightnav = document.querySelector('.right-nav')
+navlist = document.querySelector('.nav-list')
+
+
+
+burger.addEventListener('click', ()=>{
+            rightnav.classList.toggle('v-class-resp');
+            navlist.classList.toggle('v-class-resp');
+            navbar.classList.toggle('h-nav-resp');
+
+
+})
